7dbd0935b477e0404e53f2ea2f2e15a1ed1346d4
satyamx <satyamsingh748846@gmail.com>
2024-07-18 08:53:05 +0530

    changes in ui

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
 
